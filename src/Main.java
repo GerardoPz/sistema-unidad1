7ee8dc36d84b39ce7dd92aacd84b508d7f543c3b
GerardoPz <pliegolugohectorgerardo@gmail.com>
2024-09-03 19:17:53 -0600

    Menu Esqueleto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
+    static String[] opciones = {"Agregar Asociado", "Asignar Cargo", "Registrar Pago Asociado", "Imprimir Lista Asociados Directivos", "Imprimir Lista Asociados Naturales", "SALIR"};
+
     static void run() {
-        System.out.printf("Hello and welcome!");
+        String opcion;
+        do {
+            opcion = (String) JOptionPane.showInputDialog(null, "Seleccione una opcion", "Sistema Unidad 1", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+        } while(!opcion.equals("SALIR"));
     }
 
     public static void main(String[] args) {
