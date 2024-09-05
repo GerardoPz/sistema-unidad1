4b4d743ad00d99b6fe825857688636211b673578
IsmaelCR <cast.ro.ism.a@hotmail.com>
2024-09-04 19:20:00 -0600

    hoal

--- src/clasesAbs/A_Directivo.java
+++ src/clasesAbs/A_Directivo.java
@@ -9,25 +9,32 @@ public class A_Directivo extends Asociado{
 	
 	public A_Directivo() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 	public A_Directivo(int numeroSocio, String nombre, Date fechaIngreso, int numeroTelefono, String tipoAsociado, String puesto, String cargo, String fechaPosecion) {
 		super(numeroSocio, nombre, fechaIngreso,  numeroTelefono,  tipoAsociado);
-		// TODO Auto-generated constructor stub
 		this.puesto=puesto;
 		this.cargo=cargo;
 		this.fechaPosecion=fechaPosecion;
 	}
-	
+
+	public String getPuesto() {
+		return puesto;
+	}
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
 	}
 	
+	public String getCargo() {
+		return cargo;
+	}
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
 	
+	public String getFechaPosecion() {
+		return fechaPosecion;
+	}
 	public void setFechaPosecion(String fechaPosecion) {
-		fechaPosecion = fechaPosecion;
+		this.fechaPosecion = fechaPosecion;
 	}
 }
